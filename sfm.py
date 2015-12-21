f9b4a33e13fc6fe8b6623c87edd42a50397dc2cf
arokem <arokem@gmail.com>
2015-12-20 20:17:42 -0800

    More work on this. Still need to sort out and remove hard-coded paths.

--- sfm.py
+++ sfm.py
@@ -7,6 +7,7 @@ import numpy as np
 import nibabel as nib
 import dipy.reconst.sfm as sfm
 import dipy.core.gradients as grad
+import dipy.data as dpd
 
 def dpsave(img, filename):
     """
@@ -18,13 +19,13 @@ def dpsave(img, filename):
     img.set_qform(affine, 1)
     nib.save(img, filename)
 
-def calc_fa(beta, iso, mask=None):
+def calc_fa(beta, iso, gtab):
     """
     Calculate the Fiber Anistropy in each voxel sum(beta_i)/w_0
     """
-    return np.sum(beta, -1) / iso
+    return np.sum(beta, -1) / iso.predict(gtab)
 
-def calc_di(beta, mask=None):
+def calc_di(beta, sphere, mask=None):
     """
     Calculate the Dispersion Index:
 
@@ -43,55 +44,36 @@ def calc_di(beta, mask=None):
     mask_beta = beta[mask]
 
     for vox in xrange(di_fla.shape[0]):
-        inds = np.argsort(mask_beta[vox])[::-1] # From largest to
-                                                        # smallest
-        nonzero_idx = np.where(mask_beta[vox][inds]>0)
-        if len(nonzero_idx[0])>0:
+        inds = np.argsort(mask_beta[vox])[::-1]  # From largest to smallest
+        nonzero_idx = np.where(mask_beta[vox][inds] > 0)
+        if len(nonzero_idx[0]) > 0:
             # Only look at the non-zero weights:
             vox_idx = inds[nonzero_idx].astype(int)
             this_mp = mask_beta[vox][vox_idx]
-            # XXX REPLACE WITH THE SPHERE:
-            this_dirs = self.rot_vecs.T[vox_idx]
+            this_dirs = sphere.vertices[vox_idx]
             n_idx = len(vox_idx)
-            if all_to_all:
-                di_s = np.zeros(n_idx)
-                # Calculate this as all-to-all:
-                angles = np.arccos(np.dot(this_dirs, this_dirs.T))
-                for ii in xrange(n_idx):
-                    this_di_s = 0
-                    for jj in  xrange(ii+1, n_idx):
-                        ang = angles[ii, jj]
-                        di_s[ii] += np.sin(ang) * ((this_mp[ii]*this_mp[jj])/
-                                           np.sum(this_mp**2))
+            this_pdd, dirs = this_dirs[0], this_dirs[1:]
+            angles = np.arccos(np.dot(dirs, this_pdd))
+            angles = np.min(np.vstack([angles, np.pi-angles]), 0)
+            angles = angles/(np.pi/2)
+            di_flat[vox] = np.dot(this_mp[1:]**2/np.sum(this_mp**2),
+                                  np.sin(angles))
 
-                di_flat[vox] = np.mean(di_s)/n_idx
-            else:
-
-                #Calculate this from the highest peak to each one of the
-                #others:
-                this_pdd, dirs = this_dirs[0], this_dirs[1:]
-                angles = np.arccos(np.dot(dirs, this_pdd))
-                angles = np.min(np.vstack([angles, np.pi-angles]), 0)
-                angles = angles/(np.pi/2)
-                di_flat[vox] = np.dot(this_mp[1:]**2/np.sum(this_mp**2),
-                                      np.sin(angles))
-
-    out = ozu.nans(self.signal.shape[:3])
-    out[self.mask] = di_flat
+    di[mask] = di_flat
     return out
 
 
-
-if __name__=="__main__":
-    fmetadata = "/input/metadata.json"
+if __name__ == "__main__":
+    fmetadata = '/Users/arokem/tmp/input/metadata.json' #"/input/metadata.json"
     # Fetch metadata:
-    metadata = json.load(open(fmetadata))
+    with open(fmetadata, 'rt') as fobj:
+        metadata = json.load(fobj)
     fdata, fbval, fbvec = [metadata[k] for k in ["fdata", "fbval", "fbvec"]]
 
     # Load the data:
-    img = nib.load(op.join('/input', str(fdata)))
-    gtab = grad.gradient_table(op.join('/input', str(fbval)),
-                               op.join('/input', str(fbvec)))
+    img = nib.load(op.join('/Users/arokem/tmp/input', str(fdata)))
+    gtab = grad.gradient_table(op.join('/Users/arokem/tmp/input', str(fbval)),
+                               op.join('/Users/arokem/tmp/input', str(fbvec)))
     data = img.get_data()
     affine = img.get_affine()
 
@@ -107,12 +89,13 @@ if __name__=="__main__":
     sffit = sfmodel.fit(data, mask=mask)
 
     # The output will all have the same basic name as the data file-name:
-    root = op.join("/output",
+    root = op.join("/Users/arokem/tmp/output",
                    op.splitext(op.splitext(op.split(fdata)[-1])[0])[0])
 
-    # Save to file:
-    dpsave(sffit.beta, root + '_SFM_params.nii.gz')
-    sf_fa = calc_fa(sffit.beta, sffit.iso, mask=mask)
-    dpsave(sffit.beta, root + '_SFM_FA.nii.gz')
-    sf_di = calc_di(sffit.beta, mask=mask)
-    dpsave(sffit.beta, root + '_SFM_DI.nii.gz')
+    # Save to files:
+    dpsave(nib.Nifti1Image(sffit.beta, affine), root + '_SFM_params.nii.gz')
+    sf_fa = calc_fa(sffit.beta, sffit.iso, gtab)
+    dpsave(nib.Nifti1Image(sf_fa, affine), root + '_SFM_FA.nii.gz')
+    sphere = dpd.get_sphere()
+    sf_di = calc_di(sffit.beta, sphere, mask=mask)
+    dpsave(nib.Nifti1Image(sf_di, affine), root + '_SFM_DI.nii.gz')
